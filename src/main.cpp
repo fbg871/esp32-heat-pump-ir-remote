b5fe72c3274ce764f5bc0523fe3e3c88329f349e
fbg871 <fbg871@gmail.com>
2024-11-30 16:29:31 +0100

    Captured signal, trying to send

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,15 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <random>
+#include <IRremote.h>
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-
+// Display stuff
+constexpr int SCREEN_WIDTH = 128;
+constexpr int SCREEN_HEIGHT = 64;
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// IR transmitter stuff
+constexpr int SEND_PIN = 6;
 
 void setup() {
   Serial.begin(9600);
@@ -15,109 +19,82 @@ void setup() {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
-
+  display.clearDisplay();
   randomSeed(analogRead(0));
 
-  // Clear the buffer
-  display.clearDisplay();
-
-  // Display "Hello World" 
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-
-  display.setCursor(SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4);
-
-  display.println(F("Hello World!"));
-
-  display.display();
-
+  // IR transmitter setup
+  IrSender.begin(SEND_PIN);
 }
 
-uint16_t encodeRGB565(uint8_t red, uint8_t green, uint8_t blue) {
-    return ((red & 0x1F) << 11) | ((green & 0x3F) << 5) | (blue & 0x1F);
+/* 
+  Raw pulse timings for reference, split up into how I think it should be grouped:
+
+  88 87   - Frame start (timings in the 80ish range)
+  []      - Frame data (low are values below 20, high are values above 20) - 145? bits
+  110     - Frame end (timings in the 110 range)
+
+  Then a new frame starts, repeating the same signal. Sometimes this one isn't fully captured.
+*/
+
+    // 87,88,10,33,11,32,11,32,11,32,11,11,10,11,11,32,11,11,11,
+    // 10,11,11,11,11,10,11,11,32,11,32,11,11,11,32,11,11,11,10,
+    // 12,10,11,10,11,11,11,11,11,32,11,32,11,32,11,32,11,32,12,
+    // 31,12,31,11,33,11,10,12,10,11,10,12,10,11,10,11,11,11,11,
+    // 11,10,12,10,11,32,11,10,12,32,11,10,11,11,11,10,12,10,11,
+    // 10,12,10,11,10,12,10,11,11,11,10,12,10,11,32,11,32,11,32,
+    // 12,10,11,10,11,11,11,10,11,11,11,11,10,11,11,11,11,10,11,
+    // 32,11,11,11,11,10,11,11,32,12,31,11,11,11,109,
+
+    // 88,87,11,32,11,32,11,33,10,33,10,11,11,10,11,33,10,12,10,
+    // 11,11,11,10,11,11,11,10,33,10,33,10,12,10,33,10,11,11,11,
+    // 10,12,10,11,10,12,10,11,10,34,10,33,10,33,10,33,10,34,9,
+    // 34,9,34,9,34,9,12,10,12,10,12,9,12,10,12,9,12,10,12,9,12,
+    // 10,12,10,33,10,12,9,34,9,12,10,12,10,12,9,12,10,12,9,12,
+    // 10,12,9,12,10,12,10,12,9,12,10,33,10,34,9,34,9,13,8,13,9,
+    // 13,8,13,9,13,8,13,9,13,9,13,8,13,9,34,9,13,8,13,9,13,9,35,
+    // 8,35,8,13,8
+
+
+void sendSignal() {
+  // First chunk of the raw signal, with 0 omitted (which I suspect the receiver adds)
+  uint16_t signal[] = {
+    87,88,10,33,11,32,11,32,11,32,11,11,10,11,11,32,11,11,11,
+    10,11,11,11,11,10,11,11,32,11,32,11,11,11,32,11,11,11,10,
+    12,10,11,10,11,11,11,11,11,32,11,32,11,32,11,32,11,32,12,
+    31,12,31,11,33,11,10,12,10,11,10,12,10,11,10,11,11,11,11,
+    11,10,12,10,11,32,11,10,12,32,11,10,11,11,11,10,12,10,11,
+    10,12,10,11,10,12,10,11,11,11,10,12,10,11,32,11,32,11,32,
+    12,10,11,10,11,11,11,10,11,11,11,11,10,11,11,11,11,10,11,
+    32,11,11,11,11,10,11,11,32,12,31,11,11,11,109
+  };
+
+  for (int i = 0; i < sizeof(signal) / sizeof(signal[0]); i++) {
+    // boost it
+    signal[i] *= 50;
+  };
+
+
+  // Not working, unsure why (IR LED doesn't light up at all when looking through phone camera)
+  IrSender.sendRaw(signal, sizeof(signal) / sizeof(signal[0]), 38);
 }
-
-int getRandomInt(int min, int max) {
-  return random(min, max + 1);
-}
-
-void drawRandomShape() {
-    int val = getRandomInt(0, 4);
-
-    Serial.println(val);
-    display.clearDisplay();
-
-    int circleRadius = getRandomInt(10, 50);
-    int circleX = getRandomInt(10, SCREEN_WIDTH - 10);
-    int circleY = getRandomInt(10, SCREEN_HEIGHT - 10);
-
-    display.fillCircle(circleX, circleY, 10, SSD1306_WHITE);
-
-
-    // if (val == 0) {
-    //   int circleRadius = getRandomInt(10, 50);
-    //   int circleX = getRandomInt(10, SCREEN_WIDTH - 10);
-    //   int circleY = getRandomInt(10, SCREEN_HEIGHT - 10);
-
-    //   int circleRed = getRandomInt(0, 255);
-    //   int circleGreen = getRandomInt(0, 255);
-    //   int circleBlue = getRandomInt(0, 255);
-
-    //   uint16_t circleColor = encodeRGB565(circleRed, circleGreen, circleBlue);
-    //   display.drawCircle(circleX, circleY, circleRadius, circleColor);
-
-
-    // } else if (val == 1) {
-    //   // Triangle
-    //   int triangleX1 = getRandomInt(10, SCREEN_WIDTH - 10);
-    //   int triangleY1 = getRandomInt(10, SCREEN_HEIGHT - 10);
-    //   int triangleX2 = getRandomInt(10, SCREEN_WIDTH - 10);
-    //   int triangleY2 = getRandomInt(10, SCREEN_HEIGHT - 10);
-    //   int triangleX3 = getRandomInt(10, SCREEN_WIDTH - 10);
-    //   int triangleY3 = getRandomInt(10, SCREEN_HEIGHT - 10);
-
-    //   int triangleRed = getRandomInt(0, 255);
-    //   int triangleGreen = getRandomInt(0, 255);
-    //   int triangleBlue = getRandomInt(0, 255);
-
-    //   uint16_t triangleColor = encodeRGB565(triangleRed, triangleGreen, triangleBlue);
-    //   display.drawTriangle(triangleX1, triangleY1, triangleX2, triangleY2, triangleX3, 
-    //      triangleY3, triangleColor);
-
-    // } else if (val == 2) {
-
-    // } else if (val == 3) {
-
-    // }
-
-    display.display();
-
-
-}
-// variables to track x, y position of the shape
-int x = 10;
-int y = 10;
-int circleRadius = 10;
-
-int xDirection = 1;
-int yDirection = 1;
 
 void loop() {
-  display.clearDisplay();
-
-  if (x + xDirection > SCREEN_WIDTH - circleRadius || x + xDirection < 0 + circleRadius) {
-    xDirection = xDirection * -1;
+  if(Serial.available() > 0) {
+    char received = Serial.read();
+    if(received == 'S') {
+      Serial.println("Sending signal");
+      sendSignal();
+    }
   }
 
-  if(y + yDirection > SCREEN_HEIGHT - circleRadius || y + yDirection < 0 + circleRadius) {
-    yDirection = yDirection * -1;
+  // Dump raw signal for inspection
+  if (IrReceiver.decode()) {
+    int rawSignalLength = IrReceiver.decodedIRData.rawDataPtr->rawlen;
+    Serial.println("Raw data length: " + String(IrReceiver.decodedIRData.rawDataPtr->rawlen));
+
+    Serial.println("Raw data: ");
+    for (int i = 0; i < rawSignalLength; i++) {
+      Serial.print(String(IrReceiver.decodedIRData.rawDataPtr->rawbuf[i]) + " ");
+    }
   }
-
-  x += xDirection;
-  y += yDirection;
-
-
-  display.fillCircle(x, y, 10, SSD1306_WHITE);
-  display.display();
-  delay(10);
 }
